d6d12271106b95d240fbfb702bd0e9b94bcaa7da
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-07 01:06:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,14 +21,5 @@ def create_project_hash
   # return the projects hash
   projects
 end
-    
-  # projects: kickstarter.css("li.project.grid_4")
-  # project.css("h2.bbcard_name strong a").text
-  # project.css("div.project-thumbnail a img").attribute("src").value
-  # project.css("p.bbcard_blurb").text
-  # project.css("ul.project-meta").text.strip
-  # project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-  binding.pry
-end
 
 create_project_hash
